f3ef3e64449ca28173c6d2aaddfbbd54ae05d9b7
dmitry.navitski <arbalestindigo@gmail.com>
2020-02-25 19:58:14 +0300

    Initial Commit

---
+++ SplitView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitView
+//
+//  Created by Dmitry on 25.02.2020.
+//  Copyright © 2020 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
